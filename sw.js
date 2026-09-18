d8ceabd2fedcf2b9867b38d9568eadf4053f8278
Nunzio Strada <stranu.lgm@gmail.com>
2026-09-18 12:39:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * forzare l'aggiornamento). Il cambiamento di questo file fa sì che il browser
  * scarichi la nuova versione, ripulisca la vecchia cache e attivi l'update.
  */
-const CACHE_VERSION = 'v16';
+const CACHE_VERSION = 'v16.1';
 const CACHE = 'gymbro-' + CACHE_VERSION;
 
 const ASSETS = [
